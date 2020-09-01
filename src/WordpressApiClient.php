1e39008ea9f19ff0a643faac2474154c1d102e96
André Scholz (sneakyx) <info@rothaarsystems.de>
2020-09-01 17:21:42 +0200

    alpha version added

---
+++ src/WordpressApiClient.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Ontavio\WordpressApiClient;
+
+use http\Client;
+
+class WordpressApiClient{
+
+    /**
+     * @var string
+     */
+    private $basicUrl;
+
+    /**
+     * @var false|resource
+     */
+    private $curlHandler;
+
+    /**
+     * WordpressApiClient constructor.
+     * @param $username
+     * @param $password
+     */
+    public function __construct($username, $password, $basicUrl)
+    {
+        $this->httpClient=new Client();
+        // correct basicurl-> add trailing slash
+        if (substr($basicUrl,-1)!=='/'){
+            $basicUrl.="/";
+        }
+        $this->basicUrl=$basicUrl;
+
+        // login
+        $this->curlHandler = curl_init("{$basicUrl}wp-login.php");
+        curl_setopt($this->curlHandler, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($this->curlHandler, CURLOPT_COOKIESESSION, true);
+        curl_setopt($this->curlHandler, CURLOPT_HEADER, 1);
+        curl_setopt($this->curlHandler, CURLOPT_POST, 1);
+        curl_setopt($this->curlHandler, CURLOPT_POSTFIELDS,
+            "log={$username}&pwd={$password}&testcookie=1");
+        $result = curl_exec($this->curlHandler);
+
+        // get cookie
+        preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $result, $matches);
+        $cookies = $matches[1];
+
+        // set cookies for further requests
+        curl_setopt($this->curlHandler, CURLOPT_COOKIE, implode('; ', $cookies));
+    }
+
+    /**
+     * @param string $path
+     * @return bool|string
+     */
+    public function getApiData($path='posts')
+    {
+        curl_setopt_array($this->curlHandler, array(
+            CURLOPT_URL => "{$this->basicUrl}/wp-json/wp/v2/{$path}",
+            CURLOPT_POST => 0,
+            CURLOPT_HEADER => 0,
+        ));
+        $result = curl_exec($this->curlHandler);
+        return $result;
+    }
+
+}
